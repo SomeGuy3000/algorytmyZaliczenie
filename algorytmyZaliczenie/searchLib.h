790eb2740f02a3f9fbe10cdf5e2612eaf3678ebc
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2021-06-13 18:56:02 +0200

    changes in sortlib

--- algorytmyZaliczenie/searchLib.h
+++ algorytmyZaliczenie/searchLib.h
@@ -1,1 +1,50 @@
 #pragma once
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int linearSearch(int tab[], int tabSize, int x)
+{
+    int i;
+    for (i = 0; i < tabSize; i++)
+        if (tab[i] == x)
+            return i;
+    return -1;
+
+  //implementacja
+
+  /*  int result = linearSearch(tab, tabSize, wyszukiwany_element);
+      if (result == -1)
+          cout << "Element nie znajduje sie w tablicy" << endl;
+      else
+          cout << "Element znajduje sie w tablicy, pod indeksem: " << result << endl;*/
+
+}
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+
+        return binarySearch(arr, mid + 1, r, x);
+    }
+
+    return -1;
+
+    //implementacja 
+
+    /*  int result = binarySearch(tab, 0, tabSize - 1, wyszukiwany_element);
+        if (result == -1)
+            cout << "Element nie znajduje sie w tablicy" << endl;
+        else
+            cout << "Element znajduje sie w tablicy, pod indeksem: " << result << endl;*/
+
+}
