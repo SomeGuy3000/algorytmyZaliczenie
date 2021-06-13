02c1e66cbae57446524ee6541023d1447c1644d4
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2021-06-13 20:59:21 +0200

    arr=tab

--- algorytmyZaliczenie/searchLib.h
+++ algorytmyZaliczenie/searchLib.h
@@ -28,13 +28,13 @@ int binarySearch(int tab[], int l, int r, int x)
     if (r >= l) {
         int mid = l + (r - l) / 2;
 
-        if (arr[mid] == x)
+        if (tab[mid] == x)
             return mid;
 
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
+        if (tab[mid] > x)
+            return binarySearch(tab, l, mid - 1, x);
 
-        return binarySearch(arr, mid + 1, r, x);
+        return binarySearch(tab, mid + 1, r, x);
     }
 
     return -1;
