1346a39378f1cbc458835f3ed18510c5732145b8
SomeGuy3000 <lustyk12@wp.pl>
2021-06-13 16:47:59 +0200

    added alternative rand generation

--- algorytmyZaliczenie/algorytmyZaliczenie.cpp
+++ algorytmyZaliczenie/algorytmyZaliczenie.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    generateRandom(300, 100);
+    //generateRandom(300, 100);
+    generateReverseSorted(300, 100);
 }
 
--- algorytmyZaliczenie/functions.h
+++ algorytmyZaliczenie/functions.h
@@ -6,18 +6,17 @@ using namespace std;
 
 const int n = 100;
 
-int ** generateRandom(int randRange, int tabSize) {
+int * generateRandom(int randRange, int tabSize) {
 	srand(time(NULL));
-	int ** randiom = new int * [tabSize];
+	int * randiom = new int[tabSize];
 	for (int i = 0; i < tabSize; i++) {
-		randiom[i] = new int (rand() % randRange);
+		randiom[i] = rand() % randRange;
 		cout << randiom[i] << " ";
 	}
 	return randiom;
 }
 
 int generateSorted() {
-
 	int randiom[30];
 	for (int i = 0; i < 30; i++)
 	{
@@ -27,13 +26,10 @@ int generateSorted() {
 	return 0;
 }
 
-int generateReverseSorted() {
-
-	int randiom[30];
-	for (int i = 0; i < 30; i++)
-	{
-		randiom[i] = n-i;
-		cout << randiom[i] << " ";
+int generateReverseSorted(int randRange, int tabSize) {
+	int * random = generateRandom(randRange, tabSize);
+	for (int i = 0; i < tabSize; i++) {
+		cout << random[i] << " ";
 	}
 	return 0;
 }
