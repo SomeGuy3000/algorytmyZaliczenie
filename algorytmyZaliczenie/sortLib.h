7fe457863765fb507e07513fef574492ee737ad3
Kacper Pieniążek <kacpu2001@gmail.com>
2021-06-13 16:39:37 +0200

    added bubblesort

--- algorytmyZaliczenie/sortLib.h
+++ algorytmyZaliczenie/sortLib.h
@@ -1,2 +1,22 @@
 #pragma once
-fskjvghsoifg
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int bubbleSort(int tab[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (tab[j] > tab[j + 1])
+                swap(&tab[j], &tab[j + 1]);
+    return 0;
+}
