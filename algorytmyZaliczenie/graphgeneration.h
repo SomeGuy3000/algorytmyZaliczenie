d755853c4372a51579d30e028ebb063f3d188652
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2021-06-14 22:36:35 +0200

    added early graphgeneration

--- algorytmyZaliczenie/graphgeneration.h
+++ algorytmyZaliczenie/graphgeneration.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int** generateGraphUnweighted()
+{
+	srand(time(NULL));
+	int dim = 3; //nasze tabsize 
+
+	int** tab = new int* [dim];
+
+	for (int i = 0; i < dim; i++)
+	{
+		tab[i] = new int[dim];
+	}
+
+	for (int i = 0; i < dim; i++)
+	{
+		for (int j = 0; j < dim; j++)
+		{
+			tab[i][j] = 0;
+		}
+	}
+
+	return tab;
+
+	//for (int i = 0; i < dim; i++)
+	//	delete[] tab[i];
+
+	//delete[] tab; //usuwanie
+
+}
