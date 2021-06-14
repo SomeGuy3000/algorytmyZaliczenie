ad39a438e285c81b8fa695e75f6277bf16ca67cc
Kacper Pieniążek <w64885@student.wsiz.edu.pl>
2021-06-14 22:58:52 +0200

    added dijkstra and forms for graph generations

--- algorytmyZaliczenie/graphgeneration.h
+++ algorytmyZaliczenie/graphgeneration.h
@@ -6,7 +6,36 @@
 int** generateGraphUnweighted()
 {
 	srand(time(NULL));
-	int dim = 3; //nasze tabsize 
+	int dim = 3; //liczba wierzcholkow grafu
+
+	int** tab = new int* [dim];
+
+	for (int i = 0; i < dim; i++)
+	{
+		tab[i] = new int[dim];
+	}
+
+	for (int i = 0; i < dim; i++)
+	{
+		for (int j = 0; j < dim; j++)
+		{
+			tab[i][j] = 0;
+		}
+	}
+
+	return tab;
+
+	//for (int i = 0; i < dim; i++)
+	//	delete[] tab[i];
+
+	//delete[] tab; //usuwanie
+
+}
+
+int** generateGraphWeighted()
+{
+	srand(time(NULL));
+	int dim = 3; //liczba wierzcholkow grafu
 
 	int** tab = new int* [dim];
 
